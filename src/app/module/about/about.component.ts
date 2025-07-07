cc1d41fdddbd618a9ab0b230ee737296691d5702
amal <amal.abraham@wenmax.com>
2025-07-07 18:56:11 +0530

    new

--- src/app/module/about/about.component.ts
+++ src/app/module/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
@@ -8,7 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  constructor(public route:Router) {}
   public scrollToRegisterForm() {
     document.getElementById('registerForm')?.scrollIntoView({ behavior: 'smooth' });
+  }
+     public navigateTo(path: string) {
+    this.route.navigate([path]);
   }
 }
